e31859c4d2e8d8d211e600bbbb52b1472ccbf973
Sanchit-Soni <sanchitsony@gmail.com>
2021-04-15 22:45:46 +0530

    Testing player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
+import ReactPlayer from "react-player";
 function App() {
+  const [url, setUrl] = useState("a");
+  const [play, setPlay] = useState("");
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setUrl(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPlay(url);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing React Player</h1>
+      <div className="buttons">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="enter URL"
+          ></input>
+          <button>Play</button>
+        </form>
+      </div>
+      <br></br>
+      <br></br>
+
+      <div className="player">
+        <ReactPlayer
+          widht="720px"
+          height="480px"
+          controls
+          url={play}
+          onReady={() => console.log("ready")}
+          onPause={() => console.log("Paused")}
+          onEnded={() => console.log("Ended")}
+          onStart={() => console.log("Started")}
+          onPlay={() => console.log("Played")}
+          onSeek={() => console.log("Seeked")}
+        />
+      </div>
     </div>
   );
 }
