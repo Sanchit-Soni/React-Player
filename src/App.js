cc1f91330cb8593371a7f4767f45405db93470dd
Sanchit-Soni <sanchitsony@gmail.com>
2021-04-16 17:36:42 +0530

    modifications

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
+import { Button, Card, Col } from "react-bootstrap";
 
 import ReactPlayer from "react-player";
 function App() {
@@ -31,19 +32,82 @@ function App() {
       <br></br>
       <br></br>
 
-      <div className="player">
-        <ReactPlayer
-          widht="720px"
-          height="480px"
-          controls
-          url={play}
-          onReady={() => console.log("ready")}
-          onPause={() => console.log("Paused")}
-          onEnded={() => console.log("Ended")}
-          onStart={() => console.log("Started")}
-          onPlay={() => console.log("Played")}
-          onSeek={() => console.log("Seeked")}
-        />
+      <div className="parent">
+        <div className="col-1">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              // src="https://image.freepik.com/free-vector/illustration-concept-with-person-relaxing-home_52683-30923.jpg"
+            />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button onClick={() => setPlay("https://youtu.be/YykjpeuMNEk")}>
+                play
+              </Button>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              // src="https://image.freepik.com/free-vector/illustration-concept-with-person-relaxing-home_52683-30923.jpg"
+            />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button onClick={() => setPlay("https://youtu.be/AXnqkVTFUqY")}>
+                play
+              </Button>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              // src="https://image.freepik.com/free-vector/illustration-concept-with-person-relaxing-home_52683-30923.jpg"
+            />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button
+                onClick={() =>
+                  setPlay(
+                    "https://youtube.com/playlist?list=PLFD44486C45E50283"
+                  )
+                }
+              >
+                play
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="col-2">
+          <div className="player">
+            <ReactPlayer
+              widht="auto"
+              height="480px"
+              controls
+              url={play || "sad"}
+              onReady={() => console.log("ready")}
+              onPause={() => console.log("Paused")}
+              onEnded={() => console.log("Ended")}
+              onStart={() => console.log("Started")}
+              onPlay={() => console.log("Played")}
+              onSeek={() => console.log("Seeked")}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
